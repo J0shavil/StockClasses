bf76daa89ee66d2849aaeab973ba33c3c268aba5
J0shavil <142731625+J0shavil@users.noreply.github.com>
2023-09-12 12:11:29 +0100

    Initial Commit

---
+++ StockClasses/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StockClasses
+//
+//  Created by Joshua Castro on 12/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
